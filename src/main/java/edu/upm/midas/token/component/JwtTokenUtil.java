fd02d687aaeac5e1762677d9bb217f3f47c5862f
Gerardo Lagunes García <gerardolagar@ctb.upm.es>
2017-10-19 16:59:57 +0200

    Actualización de variables de entorno.

--- src/main/java/edu/upm/midas/token/component/JwtTokenUtil.java
+++ src/main/java/edu/upm/midas/token/component/JwtTokenUtil.java
@@ -36,6 +36,11 @@ public class JwtTokenUtil implements Serializable {
     @Value("${jwt.secret}")
     private String secret;
 
+    @Value("${jwt.claims.name.user}")
+    private String claim_name_user;
+    @Value("${jwt.claims.name.name}")
+    private String claim_name_name;
+
     @Value("${jwt.expiration}")
     private Long expiration;
 
@@ -95,9 +100,9 @@ public class JwtTokenUtil implements Serializable {
 
     public String generateToken(Person person, Device device) {
         Map<String, Object> claims = new HashMap<>();
-        claims.put("user", true);
-        claims.put("name", person.getFirstName() + " " + person.getLastName());
-        claims.put("secret_claim", "Perter Parker");
+        claims.put(this.claim_name_user, true);
+        claims.put(this.claim_name_name, person.getFirstName() + " " + person.getLastName());
+        //claims.put("secret_claim", "Perter Parker");
         return doGenerateToken(claims, person, generateAudience(device));
     }
 
@@ -125,7 +130,7 @@ public class JwtTokenUtil implements Serializable {
                 .parseClaimsJws(token).getBody();
 
         System.out.println("CLAIMS "+claims.toString());
-        System.out.println("EXTRACT CLAIM: " + claims.get("secret_claim"));
+        //System.out.println("EXTRACT CLAIM: " + claims.get("secret_claim"));
 
         return claims.getSubject();
 
